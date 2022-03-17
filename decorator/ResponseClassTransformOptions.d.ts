4c59fda6d4e8085f2ae1f340d1f6bc456480dff8
Renato Cargnelutti <renato.cargnelutti8@gmail.com>
2022-03-17 10:40:55 -0300

    Add compiled module

---
+++ decorator/ResponseClassTransformOptions.d.ts
@@ -0,0 +1,7 @@
+import { ClassTransformOptions } from "@nestjs/class-transformer";
+/**
+ * Options to be set to class-transformer for the result of the response.
+ */
+export declare function ResponseClassTransformOptions(
+  options: ClassTransformOptions
+): Function;
